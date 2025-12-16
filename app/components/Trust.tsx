261900f626c18364744a24e05e3a8a1d468866b5
Jazeel Zainudeen <jazeel.z@poornam.com>
2025-12-16 09:59:49 +0000

    💄 style(trust): enhance MapPin component hover effects and tooltip width

--- app/components/Trust.tsx
+++ app/components/Trust.tsx
@@ -86,15 +86,17 @@ function MapPin({ top, left, label, logo, onClick }: MapPinProps) {
           <div className="absolute inset-0 rounded-full bg-white opacity-30 animate-ping pointer-events-none" />
 
           {/* Pin circle */}
-          <div className="relative w-6 h-6 rounded-full bg-black border border-[#FFFFFF63] group-hover:bg-white group-hover:border-black transition-colors duration-300">
+          <div className="relative w-6 h-6 rounded-full bg-black border border-[#FFFFFF63] group-hover:bg-white group-hover:border-black transition-colors duration-400">
+            {/* Plus icon - visible by default */}
             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none"
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 block group-hover:hidden scale-[75%]">
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-[75%] transition-opacity duration-300 opacity-100 group-hover:opacity-0">
               <path d="M6 8H1C0.716667 8 0.479167 7.90417 0.2875 7.7125C0.0958333 7.52083 0 7.28333 0 7C0 6.71667 0.0958333 6.47917 0.2875 6.2875C0.479167 6.09583 0.716667 6 1 6H6V1C6 0.716667 6.09583 0.479167 6.2875 0.2875C6.47917 0.0958333 6.71667 0 7 0C7.28333 0 7.52083 0.0958333 7.7125 0.2875C7.90417 0.479167 8 0.716667 8 1V6H13C13.2833 6 13.5208 6.09583 13.7125 6.2875C13.9042 6.47917 14 6.71667 14 7C14 7.28333 13.9042 7.52083 13.7125 7.7125C13.5208 7.90417 13.2833 8 13 8H8V13C8 13.2833 7.90417 13.5208 7.7125 13.7125C7.52083 13.9042 7.28333 14 7 14C6.71667 14 6.47917 13.9042 6.2875 13.7125C6.09583 13.5208 6 13.2833 6 13V8Z"
                 fill="white" />
             </svg>
 
+            {/* Minus icon - visible on hover */}
             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="2" viewBox="0 0 12 2" fill="none"
-              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden group-hover:block scale-[75%]">
+              className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-[75%] transition-opacity duration-300 opacity-0 group-hover:opacity-100">
               <path d="M1 2C0.716667 2 0.479167 1.90417 0.2875 1.7125C0.0958333 1.52083 0 1.28333 0 1C0 0.716667 0.0958333 0.479167 0.2875 0.2875C0.479167 0.0958333 0.716667 0 1 0H11C11.2833 0 11.5208 0.0958333 11.7125 0.2875C11.9042 0.479167 12 0.716667 12 1C12 1.28333 11.9042 1.52083 11.7125 1.7125C11.5208 1.90417 11.2833 2 11 2H1Z" fill="black" />
             </svg>
           </div>
@@ -105,7 +107,7 @@ function MapPin({ top, left, label, logo, onClick }: MapPinProps) {
       {label && (
         <div
           ref={tooltipRef}
-          className="fixed w-[250px] flex items-start flex-col gap-2 px-6 pt-4.5 pb-3 bg-[#222222] backdrop-blur-[70px] border border-[#FFFFFF2B] rounded-[16px] rounded-bl-none shadow-lg transition-opacity duration-300 pointer-events-none whitespace-nowrap z-[9999]"
+          className="fixed w-fit flex items-start flex-col gap-2 px-6 pt-4.5 pb-3 bg-[#222222] backdrop-blur-[70px] border border-[#FFFFFF2B] rounded-[16px] rounded-bl-none shadow-lg transition-opacity duration-300 pointer-events-none whitespace-nowrap z-[9999]"
           style={{
             top: `${tooltipPosition.top}px`,
             left: `${tooltipPosition.left}px`,
