6a4d4af92794bd3b2246c87b4e20fc01e47ffda5
Jazeel Zainudeen <jazeel.z@poornam.com>
2025-12-30 10:09:40 +0000

    💄 style(footer): increase icon size for better visibility

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -270,13 +270,13 @@ export default function Footer() {
                             {service.title}
                           </h3>
                         )}
-                        <div className="relative shrink-0 size-6">
+                        <div className="relative shrink-0 size-9">
                           {isExpanded ? (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none">
                               <path d="M5 12H19" stroke="white" strokeWidth="2" strokeLinecap="round" />
                             </svg>
                           ) : (
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" viewBox="0 0 24 24" fill="none">
                               <path d="M12 5V19M5 12H19" stroke="white" strokeWidth="2" strokeLinecap="round" />
                             </svg>
                           )}
